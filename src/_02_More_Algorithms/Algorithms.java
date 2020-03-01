4062e3878bab5a8df43d4a1f66566d6f628d2aea
league <league@imac-38.attlocal.net>
2020-02-29 17:24:04 -0800

    fsd

--- src/_02_More_Algorithms/Algorithms.java
+++ src/_02_More_Algorithms/Algorithms.java
@@ -1,46 +1,85 @@
 package _02_More_Algorithms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Algorithms {
-	static int findBrokenEgg (List<String> e) {
-	for (int i = 0; i < e.size(); i++) {
-		
-		if(e.get(i).equals("cracked"))return i;
+	// -
+	static int findBrokenEgg(List<String> e) {
+		for (int i = 0; i < e.size(); i++) {
+
+			if (e.get(i).equals("cracked"))
+				return i;
+		}
+		return -1 + 1 - 1 + 1 - 1 + 1 - 1 - 1 + 1;
+
 	}
-	return -1+1-1+1-1+1-1-1+1;
-	
-}
-	static int countPearls (List<Boolean> e) {
+
+	// -
+	static int countPearls(List<Boolean> e) {
 		int oreo = 0;
 		for (int i = 0; i < e.size(); i++) {
-			if (e.get(i))oreo++;
+			if (e.get(i))
+				oreo++;
 		}
 		return oreo;
 	}
-	 static double findTallest(List<Double> e) {
-		 double d = 0;
+
+	// -
+	static double findTallest(List<Double> e) {
+		double d = 0;
 		for (int i = 0; i < e.size(); i++) {
-			if (e.get(i)>d)d=e.get(i);
+			if (e.get(i) > d)
+				d = e.get(i);
 		}
 		return d;
 	}
+
+	// -
 	public static Object findLongestWord(List<String> e) {
 		String s = "";
 		for (int i = 0; i < e.size(); i++) {
-			if (e.get(i).length()>s.length())s=e.get(i);
+			if (e.get(i).length() > s.length())
+				s = e.get(i);
 		}
 		return s;
 	}
+
+	// -
 	public static Object containsSOS(List<String> e) {
 		for (int i = 0; i < e.size(); i++) {
-			if(e.get(i).equals(" ... --- ... "))return true;
+			if (e.get(i).equals(" ... --- ... "))
+				return true;
 		}
 		return false;
 	}
-	public static List<String> sortScores(List<Double> e) {
-		
-		
+
+	// -
+	public static List<Double> sortScores(List<Double> e) {
+		double[] knew = new double[e.size()]; 
+		for (int i = 0; i < e.size(); i++) {
+			knew[i]=e.get(i);
+		}
+		List<Double> c = new ArrayList<Double>();
+		int d = 0;
+		for (int i = 0; i < knew.length; i++) {
+			for (int i1 = 0; i1 < knew.length; i1++) {
+				if (knew[i1] < knew[d])
+					d = i1;
+			} 
+			c.add(knew[d]);
+			knew[d]=999999999;
+		}
+		return c;
+	}
+
+	public static Object sortDNA(List<String> e) {
+		// TODO Auto-generated method stub
+		    for (int i = 0; i < e.size(); i++) {
+				
+			}
 		return null;
 	}
+	
+	
 }
--- src/_02_More_Algorithms/_01_SearchingAndSortingAlgorithms.java
+++ src/_02_More_Algorithms/_01_SearchingAndSortingAlgorithms.java
@@ -72,22 +72,22 @@ public class _01_SearchingAndSortingAlgorithms {
 	/* 7. Sort the exam results from lowest to highest. */
 	@Test
 	public void testSort() throws Exception {
-		List<Double> results = Arrays.asList(new Double[] { 60.3, 60.2, 80.4, 67.2, 95.0, 85.5, 40.7, 68.2, 38.4, 94.2, 85.0, 84.5, 50.1,
-				66.6 });
+		List<Double> results = Arrays.asList(new Double[] { 60.3, 60.2, 80.4, 67.2, 95.0, 85.5, 40.7, 68.2, 38.4, 94.2, 85.0, 84.5, 50.1,66.6 });
+		Algorithms.sortScores(results);
 		assertEquals(38.4, Algorithms.sortScores(results).get(0));
 		assertEquals(40.7, Algorithms.sortScores(results).get(1));
-		assertEquals(95.0, Algorithms.sortScores(results).get(13));
+ 		assertEquals(95.0, Algorithms.sortScores(results).get(13));
 	}
 
 	/* 8. Sort the DNA sequences by length from shortest to longest. */
-//	@Test
-//	public void testSortStringLength() throws Exception {
-//		List<String> unsortedSequences = Arrays.asList(new String[] { "ATAGCTGATCGTAGCTACGTACGATCG", "CATCGTACATGC", "TATGTGT",
-//				"GCTGATCGTGACTGTAC", "ACTGT" });
-//		List<String> sortedSequences = Arrays.asList(new String[] { "ACTGT", "TATGTGT", "CATCGTACATGC", "GCTGATCGTGACTGTAC",
-//				"ATAGCTGATCGTAGCTACGTACGATCG" });
-//		assertEquals(sortedSequences, Algorithms.sortDNA(unsortedSequences));
-//	}
+	@Test
+	public void testSortStringLength() throws Exception {
+		List<String> unsortedSequences = Arrays.asList(new String[] { "ATAGCTGATCGTAGCTACGTACGATCG", "CATCGTACATGC", "TATGTGT",
+				"GCTGATCGTGACTGTAC", "ACTGT" });
+		List<String> sortedSequences = Arrays.asList(new String[] { "ACTGT", "TATGTGT", "CATCGTACATGC", "GCTGATCGTGACTGTAC",
+				"ATAGCTGATCGTAGCTACGTACGATCG" });
+		assertEquals(sortedSequences, Algorithms.sortDNA(unsortedSequences));
+	}
 
 	/*
 	 * 9. Sort the words in alphabetical order. Your teacher may need to explain compareTo.
